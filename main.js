041c7884d2c3a8f2296d73de01868d53ec22266a
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-08-31 16:17:41 +0300

    completed assignment

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
+
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const hearts =document.querySelectorAll(".like")
 
+const modal = document.querySelector('#modal');
+const message = document.querySelector('#modal-message');
+for (let heart of hearts) {
+  heart.addEventListener("click", like)
+}
+
+function like(e) {
+  if (e.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      e.target.innerText = FULL_HEART;
+      e.target.classList.toggle('activated-heart');
+    })
+    .catch(err => {
+      modal.classList.toggle('hidden');
+      message.innerText = err;
+      setTimeout(() => modal.classList.toggle('hidden'), 5000);
+    })
+  }
+  if (e.target.innerText === FULL_HEART) {
+    e.target.innerText = EMPTY_HEART;
+    e.target.classList.toggle('activated-heart');
+  }
+}
 
 
 
